d061e520e276581cd37d948e94398fd90df56e6b
Tim1222 <timofeu47@gmail.com>
2022-10-02 22:54:20 +0300

    07 task

---
+++ 07.js
@@ -0,0 +1,9 @@
+// Variable declarations
+var studlyCapVaR;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVaR = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
